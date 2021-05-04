7401dbc9d1b94040c73e580df5c7057591ea4573
emmadevc <emmanuel_tm@outlook.com>
2021-05-03 20:53:47 -0500

    Agregar firmas

--- fpdf/download.php
+++ fpdf/download.php
@@ -1,76 +1,73 @@
 <?php
-require('fpdf.php');
+require('html_table.php');
 include ('../connections/conecta.php');
 $conexion = conectar_bd();
 $query="SELECT * FROM nomina WHERE direccion = '".$_GET['id']."' ORDER BY id_empleado ASC";
 $result= mysqli_query($conexion, $query);
+$query1="SELECT * FROM nomina WHERE id_universo = 's' AND direccion = '".$_GET['id']."'";
+$result1= mysqli_query($conexion, $query1);
+$row1=mysqli_fetch_array($result1);
+$query2="SELECT * FROM nomina WHERE id_universo = 'l' AND area LIKE '%apoyo administrativo%' AND direccion = '".$_GET['id']."'";
+$result2= mysqli_query($conexion, $query2);
+$row2=mysqli_fetch_array($result2);
+$query3="SELECT * FROM nomina WHERE id_universo = 'm' AND direccion = '".$_GET['id']."'";
+$result3= mysqli_query($conexion, $query3);
+$row3=mysqli_fetch_array($result3);
 
-class PDF extends FPDF
-{
-// Cabecera de página
-function Header()
-{
-    // Logo
-    $this->Image('../Imagenes/logo_solo_vc.png',10,8,15);
-    // Arial bold 15
-    $this->SetFont('Arial','B',15);
-    // Movernos a la derecha
-    $this->Cell(100);
-    // Título
-    $this->Cell(40,10,'SIRH '.$_GET['id'],0,0,'C');
-    // Salto de línea
-    $this->Ln(20);
 
-    $this->SetFont('Arial','B',5);
-    $this->Cell(7 ,10,'Id.',1,0,'C',0);
-    $this->Cell(13 ,10,'No. Plaza',1,0,'C',0);
-    $this->Cell(10 ,10,'Empleado',1,0,'C',0);
-    $this->Cell(15 ,10,'A. Paterno',1,0,'C',0);
-    $this->Cell(15 ,10,'A. Materno',1,0,'C',0);
-    $this->Cell(20 ,10,'Nombre',1,0,'C',0);
-    $this->Cell(7 ,10,'Nomi.',1,0,'C',0);
-    $this->Cell(7 ,10,'Uni.',1,0,'C',0);
-    $this->Cell(7 ,10,'Niv.',1,0,'C',0);
-    $this->Cell(10 ,10,'N. Puesto',1,0,'C',0);
-    $this->Cell(42 ,10,'Puesto',1,0,'C',0);
-    $this->Cell(7 ,10,'Sind.',1,0,'C',0);
-    $this->Cell(100 ,10,utf8_decode('Área'),1,0,'C',0);
-    $this->Cell(20 ,10,utf8_decode('Dirección'),1,1,'C',0);
-}
-
-// Pie de página
-function Footer()
-{
-    // Posición: a 1,5 cm del final
-    $this->SetY(-15);
-    // Arial italic 8
-    $this->SetFont('Arial','I',8);
-    // Número de página
-    $this->Cell(0,10,utf8_decode('Alcaldía Venustiano Carranza'),0,0,'C');
-}
-}
-
-$pdf = new PDF('L','mm','A4');
+$pdf = new CellPDF('L','mm','Letter');
 $pdf->AliasNbPages();
 $pdf->AddPage();
+$count=1;
+
 $pdf->SetFont('Arial','B',5);
-$count=0;
+$pdf->Cell(7 ,10,'ID.',1,0,'C',0);
+$pdf->Cell(13 ,10,'NO. PLAZA',1,0,'C',0);
+$pdf->Cell(12 ,10,'EMPLEADO',1,0,'C',0);
+$pdf->Cell(15 ,10,'A. PATERNO',1,0,'C',0);
+$pdf->Cell(15 ,10,'A. MATERNO',1,0,'C',0);
+$pdf->Cell(28 ,10,'NOMBRE',1,0,'C',0);
+$pdf->Cell(7 ,10,'NOMI.',1,0,'C',0);
+$pdf->Cell(7 ,10,'UNI.',1,0,'C',0);
+$pdf->Cell(7 ,10,'NIV.',1,0,'C',0);
+$pdf->Cell(10 ,10,'N. PUESTO',1,0,'C',0);
+$pdf->Cell(42 ,10,'PUESTO',1,0,'C',0);
+$pdf->Cell(7 ,10,'SIND.',1,0,'C',0);
+$pdf->Cell(73 ,10,utf8_decode('ÁREA'),1,0,'C',0);
+$pdf->Cell(20 ,10,utf8_decode('DIRECCIÓN'),1,1,'C',0);
+$pdf->SetFont('Arial','',5);
+
 while($row= mysqli_fetch_array($result)){
     $pdf->Cell(7 ,10,utf8_decode($count),1,0,'C',0);
     $pdf->Cell(13 ,10,utf8_decode($row['id_plaza']),1,0,'C',0);
-    $pdf->Cell(10 ,10,utf8_decode($row['id_empleado']),1,0,'C',0);
+    $pdf->Cell(12 ,10,utf8_decode($row['id_empleado']),1,0,'C',0);
     $pdf->Cell(15 ,10,utf8_decode($row['a_paterno']),1,0,'C',0);
     $pdf->Cell(15 ,10,utf8_decode($row['a_materno']),1,0,'C',0);
-    $pdf->Cell(20 ,10,utf8_decode($row['nombre']),1,0,'C',0);
+    $pdf->Cell(28 ,10,utf8_decode($row['nombre']),1,0,'C',0);
     $pdf->Cell(7 ,10,utf8_decode($row['id_tipo_nomina']),1,0,'C',0);
     $pdf->Cell(7 ,10,utf8_decode($row['id_universo']),1,0,'C',0);
     $pdf->Cell(7 ,10,utf8_decode($row['id_nivel_salarial']),1,0,'C',0);
     $pdf->Cell(10 ,10,utf8_decode($row['id_puesto']),1,0,'C',0);
     $pdf->Cell(42 ,10,utf8_decode($row['n_puesto']),1,0,'C',0);
     $pdf->Cell(7 ,10,utf8_decode($row['id_sindicato']),1,0,'C',0);
-    $pdf->Cell(100 ,10,utf8_decode($row['area']),1,0,'C',0);
+    $pdf->Cell(73 ,10,utf8_decode($row['area']),1,0,'C',0);
     $pdf->Cell(20 ,10,utf8_decode($row['direccion']),1,1,'C',0);
     $count++;
+}
+$pdf->Ln(20);
+$pdf->Cell(85 ,5,utf8_decode("_______________________________________________________________\n".$row1['area']." \n".$row1['nombre']." ".$row1['a_paterno']." ".$row1['a_materno']." "),0,0,'C',0);
+$pdf->Cell(85 ,5,utf8_decode("_______________________________________________________________\n".$row2['area']." \n".$row2['nombre']." ".$row2['a_paterno']." ".$row2['a_materno']." "),0,0,'C',0);
+$contador=0;
+while($row3= mysqli_fetch_array($result3)){
+    
+    if($contador==0 || ($contador % 3 == 0)){
+        $pdf->Cell(85 ,5,utf8_decode("_______________________________________________________________\n".$row3['area']." \n".$row3['nombre']." ".$row3['a_paterno']." ".$row3['a_materno']." "),0,1,'C',0);
+        $pdf->Ln(20);
+    }
+    else{
+        $pdf->Cell(85 ,5,utf8_decode("_______________________________________________________________\n".$row3['area']." \n".$row3['nombre']." ".$row3['a_paterno']." ".$row3['a_materno']." "),0,0,'C',0);
+    }
+    $contador++;
 }
 
 $pdf->Output();
--- fpdf/html_table.php
+++ fpdf/html_table.php
@@ -1,284 +1,294 @@
 <?php
-//Based on HTML2PDF by Clément Lavoillotte
-
 require('fpdf.php');
 
-//function hex2dec
-//returns an associative array (keys: R,G,B) from a hex html code (e.g. #3FE5AA)
-function hex2dec($couleur = "#000000"){
-    $R = substr($couleur, 1, 2);
-    $rouge = hexdec($R);
-    $V = substr($couleur, 3, 2);
-    $vert = hexdec($V);
-    $B = substr($couleur, 5, 2);
-    $bleu = hexdec($B);
-    $tbl_couleur = array();
-    $tbl_couleur['R']=$rouge;
-    $tbl_couleur['G']=$vert;
-    $tbl_couleur['B']=$bleu;
-    return $tbl_couleur;
-}
-
-//conversion pixel -> millimeter in 72 dpi
-function px2mm($px){
-    return $px*25.4/72;
-}
-
-function txtentities($html){
-    $trans = get_html_translation_table(HTML_ENTITIES);
-    $trans = array_flip($trans);
-    return strtr($html, $trans);
-}
-////////////////////////////////////
-
-class PDF extends FPDF
+class CellPDF extends FPDF
 {
-//variables of html parser
-protected $B;
-protected $I;
-protected $U;
-protected $HREF;
-protected $fontList;
-protected $issetfont;
-protected $issetcolor;
-
-function __construct($orientation='P', $unit='mm', $format='A4')
+    function Header()
 {
-    //Call parent constructor
-    parent::__construct($orientation,$unit,$format);
+    // Logo
+    $this->Image('../Imagenes/logo_solo_vc.png',10,8,15);
+    // Arial bold 15
+    $this->SetFont('Arial','B',15);
+    // Movernos a la derecha
+    $this->Cell(100);
+    // Título
+    $this->Cell(40,10,'SIRH '.$_GET['id'],0,0,'C');
+    // Salto de línea
+    $this->Ln(20);
 
-    //Initialization
-    $this->B=0;
-    $this->I=0;
-    $this->U=0;
-    $this->HREF='';
-
-    $this->tableborder=0;
-    $this->tdbegin=false;
-    $this->tdwidth=0;
-    $this->tdheight=0;
-    $this->tdalign="L";
-    $this->tdbgcolor=false;
-
-    $this->oldx=0;
-    $this->oldy=0;
-
-    $this->fontlist=array("arial","times","courier","helvetica","symbol");
-    $this->issetfont=false;
-    $this->issetcolor=false;
 }
 
-//////////////////////////////////////
-//html parser
-
-function WriteHTML($html)
+// Pie de página
+function Footer()
 {
-    $html=strip_tags($html,"<b><u><i><a><img><p><br><strong><em><font><tr><blockquote><hr><td><tr><table><sup>"); //remove all unsupported tags
-    $html=str_replace("\n",'',$html); //replace carriage returns with spaces
-    $html=str_replace("\t",'',$html); //replace carriage returns with spaces
-    $a=preg_split('/<(.*)>/U',$html,-1,PREG_SPLIT_DELIM_CAPTURE); //explode the string
-    foreach($a as $i=>$e)
+    // Posición: a 1,5 cm del final
+    $this->SetY(-15);
+    // Arial italic 8
+    $this->SetFont('Arial','I',8);
+    // Número de página
+    $this->Cell(0,10,utf8_decode('Alcaldía Venustiano Carranza'),0,0,'C');
+}
+
+function VCell($w, $h=0, $txt='', $border=0, $ln=0, $align='', $fill=false)
+{
+    //Output a cell
+    $k=$this->k;
+    if($this->y+$h>$this->PageBreakTrigger && !$this->InHeader && !$this->InFooter && $this->AcceptPageBreak())
     {
-        if($i%2==0)
+        //Automatic page break
+        $x=$this->x;
+        $ws=$this->ws;
+        if($ws>0)
         {
-            //Text
-            if($this->HREF)
-                $this->PutLink($this->HREF,$e);
-            elseif($this->tdbegin) {
-                if(trim($e)!='' && $e!="&nbsp;") {
-                    $this->Cell($this->tdwidth,$this->tdheight,$e,$this->tableborder,'',$this->tdalign,$this->tdbgcolor);
-                }
-                elseif($e=="&nbsp;") {
-                    $this->Cell($this->tdwidth,$this->tdheight,'',$this->tableborder,'',$this->tdalign,$this->tdbgcolor);
-                }
-            }
-            else
-                $this->Write(5,stripslashes(txtentities($e)));
+            $this->ws=0;
+            $this->_out('0 Tw');
+        }
+        $this->AddPage($this->CurOrientation,$this->CurPageSize);
+        $this->x=$x;
+        if($ws>0)
+        {
+            $this->ws=$ws;
+            $this->_out(sprintf('%.3F Tw',$ws*$k));
+        }
+    }
+    if($w==0)
+        $w=$this->w-$this->rMargin-$this->x;
+    $s='';
+// begin change Cell function 
+    if($fill || $border>0)
+    {
+        if($fill)
+            $op=($border>0) ? 'B' : 'f';
+        else
+            $op='S';
+        if ($border>1) {
+            $s=sprintf('q %.2F w %.2F %.2F %.2F %.2F re %s Q ',$border,
+                        $this->x*$k,($this->h-$this->y)*$k,$w*$k,-$h*$k,$op);
         }
         else
+            $s=sprintf('%.2F %.2F %.2F %.2F re %s ',$this->x*$k,($this->h-$this->y)*$k,$w*$k,-$h*$k,$op);
+    }
+    if(is_string($border))
+    {
+        $x=$this->x;
+        $y=$this->y;
+        if(is_int(strpos($border,'L')))
+            $s.=sprintf('%.2F %.2F m %.2F %.2F l S ',$x*$k,($this->h-$y)*$k,$x*$k,($this->h-($y+$h))*$k);
+        else if(is_int(strpos($border,'l')))
+            $s.=sprintf('q 2 w %.2F %.2F m %.2F %.2F l S Q ',$x*$k,($this->h-$y)*$k,$x*$k,($this->h-($y+$h))*$k);
+            
+        if(is_int(strpos($border,'T')))
+            $s.=sprintf('%.2F %.2F m %.2F %.2F l S ',$x*$k,($this->h-$y)*$k,($x+$w)*$k,($this->h-$y)*$k);
+        else if(is_int(strpos($border,'t')))
+            $s.=sprintf('q 2 w %.2F %.2F m %.2F %.2F l S Q ',$x*$k,($this->h-$y)*$k,($x+$w)*$k,($this->h-$y)*$k);
+        
+        if(is_int(strpos($border,'R')))
+            $s.=sprintf('%.2F %.2F m %.2F %.2F l S ',($x+$w)*$k,($this->h-$y)*$k,($x+$w)*$k,($this->h-($y+$h))*$k);
+        else if(is_int(strpos($border,'r')))
+            $s.=sprintf('q 2 w %.2F %.2F m %.2F %.2F l S Q ',($x+$w)*$k,($this->h-$y)*$k,($x+$w)*$k,($this->h-($y+$h))*$k);
+        
+        if(is_int(strpos($border,'B')))
+            $s.=sprintf('%.2F %.2F m %.2F %.2F l S ',$x*$k,($this->h-($y+$h))*$k,($x+$w)*$k,($this->h-($y+$h))*$k);
+        else if(is_int(strpos($border,'b')))
+            $s.=sprintf('q 2 w %.2F %.2F m %.2F %.2F l S Q ',$x*$k,($this->h-($y+$h))*$k,($x+$w)*$k,($this->h-($y+$h))*$k);
+    }
+    if(trim($txt)!='')
+    {
+        $cr=substr_count($txt,"\n");
+        if ($cr>0) { // Multi line
+            $txts = explode("\n", $txt);
+            $lines = count($txts);
+            for($l=0;$l<$lines;$l++) {
+                $txt=$txts[$l];
+                $w_txt=$this->GetStringWidth($txt);
+                if ($align=='U')
+                    $dy=$this->cMargin+$w_txt;
+                elseif($align=='D')
+                    $dy=$h-$this->cMargin;
+                else
+                    $dy=($h+$w_txt)/2;
+                $txt=str_replace(')','\\)',str_replace('(','\\(',str_replace('\\','\\\\',$txt)));
+                if($this->ColorFlag)
+                    $s.='q '.$this->TextColor.' ';
+                $s.=sprintf('BT 0 1 -1 0 %.2F %.2F Tm (%s) Tj ET ',
+                    ($this->x+.5*$w+(.7+$l-$lines/2)*$this->FontSize)*$k,
+                    ($this->h-($this->y+$dy))*$k,$txt);
+                if($this->ColorFlag)
+                    $s.=' Q ';
+            }
+        }
+        else { // Single line
+            $w_txt=$this->GetStringWidth($txt);
+            $Tz=100;
+            if ($w_txt>$h-2*$this->cMargin) {
+                $Tz=($h-2*$this->cMargin)/$w_txt*100;
+                $w_txt=$h-2*$this->cMargin;
+            }
+            if ($align=='U')
+                $dy=$this->cMargin+$w_txt;
+            elseif($align=='D')
+                $dy=$h-$this->cMargin;
+            else
+                $dy=($h+$w_txt)/2;
+            $txt=str_replace(')','\\)',str_replace('(','\\(',str_replace('\\','\\\\',$txt)));
+            if($this->ColorFlag)
+                $s.='q '.$this->TextColor.' ';
+            $s.=sprintf('q BT 0 1 -1 0 %.2F %.2F Tm %.2F Tz (%s) Tj ET Q ',
+                        ($this->x+.5*$w+.3*$this->FontSize)*$k,
+                        ($this->h-($this->y+$dy))*$k,$Tz,$txt);
+            if($this->ColorFlag)
+                $s.=' Q ';
+        }
+    }
+// end change Cell function 
+    if($s)
+        $this->_out($s);
+    $this->lasth=$h;
+    if($ln>0)
+    {
+        //Go to next line
+        $this->y+=$h;
+        if($ln==1)
+            $this->x=$this->lMargin;
+    }
+    else
+        $this->x+=$w;
+}
+
+function Cell($w, $h=0, $txt='', $border=0, $ln=0, $align='', $fill=false, $link='')
+{
+    //Output a cell
+    $k=$this->k;
+    if($this->y+$h>$this->PageBreakTrigger && !$this->InHeader && !$this->InFooter && $this->AcceptPageBreak())
+    {
+        //Automatic page break
+        $x=$this->x;
+        $ws=$this->ws;
+        if($ws>0)
         {
-            //Tag
-            if($e[0]=='/')
-                $this->CloseTag(strtoupper(substr($e,1)));
+            $this->ws=0;
+            $this->_out('0 Tw');
+        }
+        $this->AddPage($this->CurOrientation,$this->CurPageSize);
+        $this->x=$x;
+        if($ws>0)
+        {
+            $this->ws=$ws;
+            $this->_out(sprintf('%.3F Tw',$ws*$k));
+        }
+    }
+    if($w==0)
+        $w=$this->w-$this->rMargin-$this->x;
+    $s='';
+// begin change Cell function
+    if($fill || $border>0)
+    {
+        if($fill)
+            $op=($border>0) ? 'B' : 'f';
+        else
+            $op='S';
+        if ($border>1) {
+            $s=sprintf('q %.2F w %.2F %.2F %.2F %.2F re %s Q ',$border,
+                $this->x*$k,($this->h-$this->y)*$k,$w*$k,-$h*$k,$op);
+        }
+        else
+            $s=sprintf('%.2F %.2F %.2F %.2F re %s ',$this->x*$k,($this->h-$this->y)*$k,$w*$k,-$h*$k,$op);
+    }
+    if(is_string($border))
+    {
+        $x=$this->x;
+        $y=$this->y;
+        if(is_int(strpos($border,'L')))
+            $s.=sprintf('%.2F %.2F m %.2F %.2F l S ',$x*$k,($this->h-$y)*$k,$x*$k,($this->h-($y+$h))*$k);
+        else if(is_int(strpos($border,'l')))
+            $s.=sprintf('q 2 w %.2F %.2F m %.2F %.2F l S Q ',$x*$k,($this->h-$y)*$k,$x*$k,($this->h-($y+$h))*$k);
+            
+        if(is_int(strpos($border,'T')))
+            $s.=sprintf('%.2F %.2F m %.2F %.2F l S ',$x*$k,($this->h-$y)*$k,($x+$w)*$k,($this->h-$y)*$k);
+        else if(is_int(strpos($border,'t')))
+            $s.=sprintf('q 2 w %.2F %.2F m %.2F %.2F l S Q ',$x*$k,($this->h-$y)*$k,($x+$w)*$k,($this->h-$y)*$k);
+        
+        if(is_int(strpos($border,'R')))
+            $s.=sprintf('%.2F %.2F m %.2F %.2F l S ',($x+$w)*$k,($this->h-$y)*$k,($x+$w)*$k,($this->h-($y+$h))*$k);
+        else if(is_int(strpos($border,'r')))
+            $s.=sprintf('q 2 w %.2F %.2F m %.2F %.2F l S Q ',($x+$w)*$k,($this->h-$y)*$k,($x+$w)*$k,($this->h-($y+$h))*$k);
+        
+        if(is_int(strpos($border,'B')))
+            $s.=sprintf('%.2F %.2F m %.2F %.2F l S ',$x*$k,($this->h-($y+$h))*$k,($x+$w)*$k,($this->h-($y+$h))*$k);
+        else if(is_int(strpos($border,'b')))
+            $s.=sprintf('q 2 w %.2F %.2F m %.2F %.2F l S Q ',$x*$k,($this->h-($y+$h))*$k,($x+$w)*$k,($this->h-($y+$h))*$k);
+    }
+    if (trim($txt)!='') {
+        $cr=substr_count($txt,"\n");
+        if ($cr>0) { // Multi line
+            $txts = explode("\n", $txt);
+            $lines = count($txts);
+            for($l=0;$l<$lines;$l++) {
+                $txt=$txts[$l];
+                $w_txt=$this->GetStringWidth($txt);
+                if($align=='R')
+                    $dx=$w-$w_txt-$this->cMargin;
+                elseif($align=='C')
+                    $dx=($w-$w_txt)/2;
+                else
+                    $dx=$this->cMargin;
+
+                $txt=str_replace(')','\\)',str_replace('(','\\(',str_replace('\\','\\\\',$txt)));
+                if($this->ColorFlag)
+                    $s.='q '.$this->TextColor.' ';
+                $s.=sprintf('BT %.2F %.2F Td (%s) Tj ET ',
+                    ($this->x+$dx)*$k,
+                    ($this->h-($this->y+.5*$h+(.7+$l-$lines/2)*$this->FontSize))*$k,
+                    $txt);
+                if($this->underline)
+                    $s.=' '.$this->_dounderline($this->x+$dx,$this->y+.5*$h+.3*$this->FontSize,$txt);
+                if($this->ColorFlag)
+                    $s.=' Q ';
+                if($link)
+                    $this->Link($this->x+$dx,$this->y+.5*$h-.5*$this->FontSize,$w_txt,$this->FontSize,$link);
+            }
+        }
+        else { // Single line
+            $w_txt=$this->GetStringWidth($txt);
+            $Tz=100;
+            if ($w_txt>$w-2*$this->cMargin) { // Need compression
+                $Tz=($w-2*$this->cMargin)/$w_txt*100;
+                $w_txt=$w-2*$this->cMargin;
+            }
+            if($align=='R')
+                $dx=$w-$w_txt-$this->cMargin;
+            elseif($align=='C')
+                $dx=($w-$w_txt)/2;
             else
-            {
-                //Extract attributes
-                $a2=explode(' ',$e);
-                $tag=strtoupper(array_shift($a2));
-                $attr=array();
-                foreach($a2 as $v)
-                {
-                    if(preg_match('/([^=]*)=["\']?([^"\']*)/',$v,$a3))
-                        $attr[strtoupper($a3[1])]=$a3[2];
-                }
-                $this->OpenTag($tag,$attr);
-            }
+                $dx=$this->cMargin;
+            $txt=str_replace(')','\\)',str_replace('(','\\(',str_replace('\\','\\\\',$txt)));
+            if($this->ColorFlag)
+                $s.='q '.$this->TextColor.' ';
+            $s.=sprintf('q BT %.2F %.2F Td %.2F Tz (%s) Tj ET Q ',
+                        ($this->x+$dx)*$k,
+                        ($this->h-($this->y+.5*$h+.3*$this->FontSize))*$k,
+                        $Tz,$txt);
+            if($this->underline)
+                $s.=' '.$this->_dounderline($this->x+$dx,$this->y+.5*$h+.3*$this->FontSize,$txt);
+            if($this->ColorFlag)
+                $s.=' Q ';
+            if($link)
+                $this->Link($this->x+$dx,$this->y+.5*$h-.5*$this->FontSize,$w_txt,$this->FontSize,$link);
         }
     }
+// end change Cell function
+    if($s)
+        $this->_out($s);
+    $this->lasth=$h;
+    if($ln>0)
+    {
+        //Go to next line
+        $this->y+=$h;
+        if($ln==1)
+            $this->x=$this->lMargin;
+    }
+    else
+        $this->x+=$w;
 }
-
-function OpenTag($tag, $attr)
-{
-    //Opening tag
-    switch($tag){
-
-        case 'SUP':
-            if( !empty($attr['SUP']) ) {    
-                //Set current font to 6pt     
-                $this->SetFont('','',6);
-                //Start 125cm plus width of cell to the right of left margin         
-                //Superscript "1" 
-                $this->Cell(2,2,$attr['SUP'],0,0,'L');
-            }
-            break;
-
-        case 'TABLE': // TABLE-BEGIN
-            if( !empty($attr['BORDER']) ) $this->tableborder=$attr['BORDER'];
-            else $this->tableborder=0;
-            break;
-        case 'TR': //TR-BEGIN
-            break;
-        case 'TD': // TD-BEGIN
-            if( !empty($attr['WIDTH']) ) $this->tdwidth=($attr['WIDTH']/4);
-            else $this->tdwidth=40; // Set to your own width if you need bigger fixed cells
-            if( !empty($attr['HEIGHT']) ) $this->tdheight=($attr['HEIGHT']/6);
-            else $this->tdheight=6; // Set to your own height if you need bigger fixed cells
-            if( !empty($attr['ALIGN']) ) {
-                $align=$attr['ALIGN'];        
-                if($align=='LEFT') $this->tdalign='L';
-                if($align=='CENTER') $this->tdalign='C';
-                if($align=='RIGHT') $this->tdalign='R';
-            }
-            else $this->tdalign='L'; // Set to your own
-            if( !empty($attr['BGCOLOR']) ) {
-                $coul=hex2dec($attr['BGCOLOR']);
-                    $this->SetFillColor($coul['R'],$coul['G'],$coul['B']);
-                    $this->tdbgcolor=true;
-                }
-            $this->tdbegin=true;
-            break;
-
-        case 'HR':
-            if( !empty($attr['WIDTH']) )
-                $Width = $attr['WIDTH'];
-            else
-                $Width = $this->w - $this->lMargin-$this->rMargin;
-            $x = $this->GetX();
-            $y = $this->GetY();
-            $this->SetLineWidth(0.2);
-            $this->Line($x,$y,$x+$Width,$y);
-            $this->SetLineWidth(0.2);
-            $this->Ln(1);
-            break;
-        case 'STRONG':
-            $this->SetStyle('B',true);
-            break;
-        case 'EM':
-            $this->SetStyle('I',true);
-            break;
-        case 'B':
-        case 'I':
-        case 'U':
-            $this->SetStyle($tag,true);
-            break;
-        case 'A':
-            $this->HREF=$attr['HREF'];
-            break;
-        case 'IMG':
-            if(isset($attr['SRC']) && (isset($attr['WIDTH']) || isset($attr['HEIGHT']))) {
-                if(!isset($attr['WIDTH']))
-                    $attr['WIDTH'] = 0;
-                if(!isset($attr['HEIGHT']))
-                    $attr['HEIGHT'] = 0;
-                $this->Image($attr['SRC'], $this->GetX(), $this->GetY(), px2mm($attr['WIDTH']), px2mm($attr['HEIGHT']));
-            }
-            break;
-        case 'BLOCKQUOTE':
-        case 'BR':
-            $this->Ln(5);
-            break;
-        case 'P':
-            $this->Ln(10);
-            break;
-        case 'FONT':
-            if (isset($attr['COLOR']) && $attr['COLOR']!='') {
-                $coul=hex2dec($attr['COLOR']);
-                $this->SetTextColor($coul['R'],$coul['G'],$coul['B']);
-                $this->issetcolor=true;
-            }
-            if (isset($attr['FACE']) && in_array(strtolower($attr['FACE']), $this->fontlist)) {
-                $this->SetFont(strtolower($attr['FACE']));
-                $this->issetfont=true;
-            }
-            if (isset($attr['FACE']) && in_array(strtolower($attr['FACE']), $this->fontlist) && isset($attr['SIZE']) && $attr['SIZE']!='') {
-                $this->SetFont(strtolower($attr['FACE']),'',$attr['SIZE']);
-                $this->issetfont=true;
-            }
-            break;
-    }
 }
-
-function CloseTag($tag)
-{
-    //Closing tag
-    if($tag=='SUP') {
-    }
-
-    if($tag=='TD') { // TD-END
-        $this->tdbegin=false;
-        $this->tdwidth=0;
-        $this->tdheight=0;
-        $this->tdalign="L";
-        $this->tdbgcolor=false;
-    }
-    if($tag=='TR') { // TR-END
-        $this->Ln();
-    }
-    if($tag=='TABLE') { // TABLE-END
-        $this->tableborder=0;
-    }
-
-    if($tag=='STRONG')
-        $tag='B';
-    if($tag=='EM')
-        $tag='I';
-    if($tag=='B' || $tag=='I' || $tag=='U')
-        $this->SetStyle($tag,false);
-    if($tag=='A')
-        $this->HREF='';
-    if($tag=='FONT'){
-        if ($this->issetcolor==true) {
-            $this->SetTextColor(0);
-        }
-        if ($this->issetfont) {
-            $this->SetFont('arial');
-            $this->issetfont=false;
-        }
-    }
-}
-
-function SetStyle($tag, $enable)
-{
-    //Modify style and select corresponding font
-    $this->$tag+=($enable ? 1 : -1);
-    $style='';
-    foreach(array('B','I','U') as $s) {
-        if($this->$s>0)
-            $style.=$s;
-    }
-    $this->SetFont('',$style);
-}
-
-function PutLink($URL, $txt)
-{
-    //Put a hyperlink
-    $this->SetTextColor(0,0,255);
-    $this->SetStyle('U',true);
-    $this->Write(5,$txt,$URL);
-    $this->SetStyle('U',false);
-    $this->SetTextColor(0);
-}
-
-}//end of class
-
 ?>
